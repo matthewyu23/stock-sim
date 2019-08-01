044090dd6360a612710aeadf8333b19fac96199d
Matthew <matthew.yu007@gmail.com>
2019-08-01 14:52:53 -0700

    buy and sell working, math working, adding and removing shares working, fixed #1 fixed #2

--- stock_sim.py
+++ stock_sim.py
@@ -1,11 +1,10 @@
 import requests
 import json
 import pickle
-import threading
 
 watchList = {} #initial watchlist for first time loggin
 positions = {}
-wealth = []
+money = [0, 10000]
 
 def openWPickle(): #reassigns watchList to data in pickle file
     pickleIn = open("watchList.pickle", "rb")
@@ -25,6 +24,15 @@ def savePPickle(x):
     pickle.dump(x, pickleOut)
     pickleOut.close()
 
+def openMPickle(): 
+    pickleIn = open("money.pickle", "rb")
+    return pickle.load(pickleIn)
+
+def saveMPickle(x): 
+    pickleOut = open("money.pickle", "wb")
+    pickle.dump(x, pickleOut)
+    pickleOut.close()
+
 
 try: #opens a pickle file if there is one, creates one otherwize
     watchList = openWPickle()
@@ -36,6 +44,11 @@ try:
 except (Exception) as e:
     savePPickle(watchList)
 
+try: 
+    money = openMPickle()
+except (Exception) as e:
+    saveMPickle(money)
+
 
 def updatePrices(): 
     print("updating prices")
@@ -45,14 +58,22 @@ def updatePrices():
         response = requests.get(f"https://cloud.iexapis.com/stable/stock/{x}/quote?token=pk_520e6bf649924304a029ffc1d880fd0e")
         watchList[x.upper()] = "$" + str(response.json()["latestPrice"]) 
 
+def calculateInvested(): 
+    print("updating wealth")
+    money[0] = 0
+    for x in list(positions): 
+        if len(positions) > 0: 
+            x = x.lower()
+            money[0] = money[0] + float(positions[x.upper()]) * float(requests.get(f"https://cloud.iexapis.com/stable/stock/{x}/quote?token=pk_520e6bf649924304a029ffc1d880fd0e").json()["latestPrice"])
 while True: 
 
-    
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     updatePrices()
+    calculateInvested()
     print("Watch List: "+ str(watchList))
     print("Positions: " + str(positions))
-    print("Wealth: " + wealth)
+    print("Invested: " + str(round(money[0], 2)))
+    print("Uninvested: " + str(round(money[1], 2)))
 
 
     userInput = input("ADD/REMOVE/BUY/SELL: ").lower() #checking user input
@@ -64,15 +85,39 @@ while True:
         del watchList[input("TICKER: ").upper()]
         saveWPickle(watchList)
     elif userInput == "buy": 
-        positions.update({input("TICKER: ").upper() : input("HOW MANY SHARES: ") + " shares"})
-        savePPickle(positions)
+        toBuy = input("TICKER: ")
+        numberOfSharesToBuy = input("HOW MANY SHARES: ")
+        transaction = float(numberOfSharesToBuy) * float(requests.get(f"https://cloud.iexapis.com/stable/stock/{toBuy.lower()}/quote?token=pk_520e6bf649924304a029ffc1d880fd0e").json()["latestPrice"])
+        if transaction <= money[1]: 
+            money[1] = money [1] - transaction
+            saveMPickle(money)
+            try: 
+                positions[toBuy.upper()] = str(int(positions[toBuy.upper()]) + int(numberOfSharesToBuy))
+                savePPickle(positions)
+            except (Exception): 
+                positions.update({toBuy.upper() : numberOfSharesToBuy})
+                savePPickle(positions)
     elif userInput == "sell": 
-        del positions[input("TICKER: ").upper()]
-        savePPickle(positions)
+        toSell = input("TICKER: ")
+        numberOfSharesToSell = input("Shares: ")
+        try: 
+            if int(numberOfSharesToSell) <= int(positions[toSell.upper()]): 
+                transaction = float(numberOfSharesToSell) * float(requests.get(f"https://cloud.iexapis.com/stable/stock/{toSell.lower()}/quote?token=pk_520e6bf649924304a029ffc1d880fd0e").json()["latestPrice"])
+                money[0] = money[0] - transaction
+                money[1] = money[1] + transaction
+                saveMPickle(money)
+                positions[toSell.upper()] = str(int(positions[toSell.upper()]) - int(numberOfSharesToSell))
+                if positions[toSell.upper()] == "0": 
+                    del positions[toSell.upper()]
+                savePPickle(positions)
+        except (Exception): 
+            print("You don't own this stock")
     elif userInput == "reset": 
         watchList = {}
         positions = {}
+        money = [0, 10000]
         saveWPickle(watchList)
         savePPickle(positions)
+        saveMPickle(money)
     else: 
         print("Invalid command")
